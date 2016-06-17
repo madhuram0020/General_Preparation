4116791f2893e996caeab242373c27b46061bd5f
manish verma <manish verma>
2016-06-18 02:44:52 +0530

    third push after registration api call.

--- app/src/main/java/sked/com/generalpreparation/fragment/RegisterFragment.java
+++ app/src/main/java/sked/com/generalpreparation/fragment/RegisterFragment.java
@@ -1,6 +1,8 @@
 package sked.com.generalpreparation.fragment;
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,39 +11,110 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import sked.com.generalpreparation.LoginActivity;
 import sked.com.generalpreparation.R;
+import sked.com.generalpreparation.Synchronize;
+import sked.com.generalpreparation.utils.Utils;
 
 /**
  * A simple {@link Fragment} subclass of LoginActivity.
  */
 public class RegisterFragment extends Fragment {
+
+    final String QUERY_PARAM = "name";
+    final String EMAIL_PARAM = "email";
+    final String PASSWORD_PARAM = "password";
+    URL url;
     /*find id of all views and instantiate them*/
     @BindView(R.id.etName)
     EditText nameField;
     @BindView(R.id.etEmail)
     EditText emailField;
-    @BindView(R.id.etPhone)
-    EditText mobileField;
     @BindView(R.id.etPwd)
     EditText passwordField;
-   @OnClick(R.id.registerBtn) void registerUser(){
-       Toast.makeText(getActivity(),"success",Toast.LENGTH_LONG).show();
-   }
 
     public RegisterFragment() {
         // Required empty public constructor
     }
 
+    @OnClick(R.id.registerBtn)
+    void registerUser() {
+        final String userName = nameField.getText().toString().toLowerCase().trim();
+        final String userEmail = emailField.getText().toString().toLowerCase().trim();
+        final String userPassword = passwordField.getText().toString().toLowerCase().trim();
+
+/*append parameter in the url*/
+
+        Uri builtUri = Uri.parse(Synchronize.URL_REGISTER)
+                .buildUpon()
+                .appendQueryParameter(QUERY_PARAM, userName)
+                .appendQueryParameter(EMAIL_PARAM, userEmail)
+                .appendQueryParameter(PASSWORD_PARAM, "" + Integer.parseInt(userPassword))//check how to append integer
+                .build();
+        try {
+            url = new URL(builtUri.toString());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        StringRequest request = new StringRequest(Request.Method.POST, url.toString(), new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                try {
+                    JSONObject jsonObject = new JSONObject(response);
+
+                        try {
+                            if (jsonObject.getString("error_msg").equalsIgnoreCase("User already existed with "+userEmail)) {
+                                Toast.makeText(getActivity(),"All ready Registered",Toast.LENGTH_LONG).show();
+
+                            } else{
+                                Toast.makeText(getActivity(),"Success",Toast.LENGTH_LONG).show();
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+
+        RequestQueue requestQueue= Volley.newRequestQueue(getActivity());
+        requestQueue.add(request);
+      //  Toast.makeText(getActivity(), "success", Toast.LENGTH_LONG).show();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View v=inflater.inflate(R.layout.fragment_register, container, false);
-        ButterKnife.bind(this,v);
+        View v = inflater.inflate(R.layout.fragment_register, container, false);
+        ButterKnife.bind(this, v);
+
 
         return v;
     }
---
+++ app/src/main/java/sked/com/generalpreparation/synchronize.java
@@ -0,0 +1,9 @@
+package sked.com.generalpreparation;
+
+/**
+ * Created by verma on 6/18/2016.
+ */
+public class Synchronize {
+
+    public static final String URL_REGISTER="http://www.ebiharportal.com/api/register.php?";
+}
